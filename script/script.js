88dea23f6a4053ff1451104c34ba5112e1d08f7a
Maxim Marchenko <slayer.jk@mail.ru>
2017-01-15 17:57:53 +0600

    Work on feedback(html/css/js); some fixes for site done.

--- script/script.js
+++ script/script.js
@@ -1,26 +1,38 @@
-"use strict";
-
 $(document).ready(function () {
+  "use strict";
   
-// fade effect on slides in slider-------
+  //feedback-modal-window
+  $('.feedback-js').click(function () {
+    event.preventDefault();
+    
+    $('.feedback').addClass('feedback_show');
+    
+  });
+  
+  $('.feedback__close').click(function () {
+    $('.feedback').hide(function () {
+      $(this).removeClass('feedback_show');
+    });
+  });
+  
+  // fade effect on slides in slider-------
   $.fn.slidesSwitch = function () {
     $(".slider__slide").fadeToggle(1000);
   };
-  
+
   $('.slider__switch').click(function () {
     $(document).slidesSwitch();
   });
+
+  setInterval($.fn.slidesSwitch, 4000);
   
-  setInterval( $.fn.slidesSwitch, 4000 );
- //---------------------------------------
-  
-// test button on page for va tests------
+  // test button on page for va tests------
   $.fn.testFunction = function () {
-    $(".main-menu").fadeToggle('slow');
+    $(".main-menu").effect('bounce', 1000);
   };
 
   $('.js-test-btn').click(function () {
     $(document).testFunction();
   });
-//----------------------------------------
+  //----------------------------------------
 });
