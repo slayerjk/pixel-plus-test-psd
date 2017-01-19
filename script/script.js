7bc4977f822dd4eb04b4de3ed72a45bb0430b399
Maxim Marchenko <slayer.jk@mail.ru>
2017-01-19 07:30:00 +0600

    Overlay added(makes feddback be a modal-window; corresponding js/html/css; some code for feedback-error behaviour.

--- script/script.js
+++ script/script.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
   
   //feedback-modal-window-with-fade-effect
   $.fn.toggleForm = function () {
+    $('.overlay').toggle();
     $('.feedback').fadeToggle('slow');
   };
   
@@ -11,6 +12,13 @@ $(document).ready(function () {
     $.fn.toggleForm();
   });
   
+  $(document).on('click', '.feedback__submit', function () {
+    if (!$('#user-name') || !$('#user-phone') || !$('#user-email')) {
+      event.preventDefault();
+      $('.feedback__input_required').toggleClass('feedback__input_error').prop('placeholder', 'Поле обязательно для заполнения');
+    }
+  });
+      
   // fade effect on slides in slider-------
   $.fn.slidesSwitch = function () {
     $(".slider__slide").fadeToggle(1000);
